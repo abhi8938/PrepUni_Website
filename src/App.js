5673e4a212ab7cb5e80ffaf2f70ce603f01b13e0
Abhishek Singh <abhishek8938@gamil.com>
2021-06-23 14:07:09 +0530

    first commmit

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import "@vetixy/circular-std";
 import Contact from "./pages/contact";
 import Home from "./pages/home";
 import Middle from "./pages/middle";
+import ReactGA from "react-ga";
 import logo from "./logo.svg";
 
 function App() {
@@ -12,6 +13,12 @@ function App() {
       <Home />
       <Middle />
       <Contact />
+      {ReactGA.initialize("G-LGTNF3TRYM", { debug: true })}
+      <pre>
+        ReactGA.initialize(
+        {this.renderConfigs()}
+        );
+      </pre>
     </div>
   );
 }
